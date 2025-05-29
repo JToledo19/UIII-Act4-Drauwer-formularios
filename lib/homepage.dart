3dc2d11e13f9c9a6cb7279640641a5c27816fe53
a.22308051281106 <a.22308051281106@cbtis128.edu.mx>
2025-05-29 15:52:59 +0000

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.2,
+        title: Text("Formulario Tablas"),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 18, 134, 243),
+        actions: <Widget>[
+          InkWell(
+            onTap: null,
+            child:  IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ), onPressed: () {  },
+            ),
+          ),
+          InkWell(
+            onTap: null,
+            child:  IconButton(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ), onPressed: () {  },
+            ),
+          ),
+        ],
+      ),
+
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: const Color.fromARGB(255, 19, 217, 252)),
+              accountName: Text("Jorge Antonio Toledo" ), 
+              accountEmail: Text("JToledo@gmail.com"), 
+              currentAccountPicture: GestureDetector( 
+                child: CircleAvatar(
+                  child: Text(
+                    "AM",
+                    style: TextStyle(
+                        color: Colors.pink,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  backgroundColor: Colors.white,
+                ),
+
+              ),           
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ recursos/lemee.md
@@ -0,0 +1,5 @@
+## AppBar + Drawer
+(![alt text](image.png))
+
+## EncabezadoDrauw
+![alt text](image-1.png)
